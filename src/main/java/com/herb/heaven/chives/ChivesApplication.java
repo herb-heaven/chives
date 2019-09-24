ff98c0cd1be00cde4b40fce0453b111c8f31c9ec
longxw <longxw@minstone.com.cn>
2019-09-24 14:13:43 +0800

    init project

---
+++ src/main/java/com/herb/heaven/chives/ChivesApplication.java
@@ -0,0 +1,13 @@
+package com.herb.heaven.chives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChivesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herb/heaven/chives/ChivesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.herb.heaven.chives;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChivesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
